5284a2ecb69b6f019576d23e2a59629594dff9d9
cicu90 <carlacardozo990@gmail.com>
2022-04-04 16:19:17 +0200

    create structure mvc

---
+++ src/pages/DashboardProducts/DashboardProducts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './DashboardProducts.scss';
+
+const DashboardProducts = () => (
+  <div className="DashboardProducts">
+    DashboardProducts Component
+  </div>
+);
+
+DashboardProducts.propTypes = {};
+
+DashboardProducts.defaultProps = {};
+
+export default DashboardProducts;
