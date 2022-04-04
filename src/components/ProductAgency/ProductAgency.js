4842bcae0335b9dfbd75bf470da9e0c64c2fc098
cicu90 <carlacardozo990@gmail.com>
2022-04-04 16:08:30 +0200

    init redux-property

---
+++ src/components/ProductAgency/ProductAgency.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ProductAgency.scss';
+
+const ProductAgency = () => (
+  <div className="ProductAgency">
+    ProductAgency Component
+  </div>
+);
+
+ProductAgency.propTypes = {};
+
+ProductAgency.defaultProps = {};
+
+export default ProductAgency;
